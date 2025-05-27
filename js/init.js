777a536aaba4b74421c96f12c725b758f1b12611
vilivonka <gromootvod@lenta.ru>
2025-05-27 17:51:16 +0300

    first commit

---
+++ js/init.js
@@ -0,0 +1,50 @@
+$('.main_slider').slick({
+  dots: true,
+  infinite: true,
+  arrows: false,
+  speed: 700,
+  slidesToShow: 1,
+  adaptiveHeight: false,
+  autoplay: true,
+});
+
+
+$('.credit_box').slick({
+  dots: true,
+  infinite: true,
+  arrows: false,
+  speed: 700,
+  slidesToShow: 4,
+  adaptiveHeight: false,
+  autoplay: true,
+});
+
+
+$('.stages_box_item_slider').slick({
+  dots: false,
+  infinite: true,
+  arrows: false,
+  speed: 700,
+  slidesToShow: 1,
+  adaptiveHeight: false,
+  autoplay:false,
+});
+
+$('.video_slider').slick({
+  dots: true,
+  infinite: true,
+  arrows: false,
+  speed: 700,
+  slidesToShow: 3,
+  adaptiveHeight: false,
+  autoplay: true,
+});
+$('.reviews_slider').slick({
+  dots: false,
+  infinite: true,
+  arrows: false,
+  speed: 700,
+  slidesToShow: 1,
+  adaptiveHeight: false,
+  autoplay:false,
+});
